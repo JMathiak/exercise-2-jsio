b9b736dec9115cc64e328aca5f5e9625dae4429f
Josh Mathiak <jmathiak4@gmail.com>
2022-11-09 13:23:55 -0600

    added header + navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
+    fontFamily: {
+      sans: ['"Trebuchet MS"', "sans-serif"],
+    },
+    colors: {
+      background: "#f5f4ef",
+      black: "#000305",
+      white: "#fff",
+      primaryred: "#c74350",
+    },
     extend: {},
   },
   plugins: [],
